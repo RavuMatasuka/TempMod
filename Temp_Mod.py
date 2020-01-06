6c6dd370b4217acec288dd29877c9bb663d2f3d9
RavuMatasuka <57275139+RavuMatasuka@users.noreply.github.com>
2020-01-06 16:25:47 +1300

    Create Temp_Mod.py

---
+++ Temp_Mod.py
@@ -0,0 +1,13 @@
+Celsius = int(raw_input("Enter Celsius Temp to Convert to Fahrenheit : "))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"
+
+
+
+Fahrenheit = int(raw_input("Enter Fahrenheit Temp to Convert to Celsius : "))
+
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+
+print "Temperature:", Fahrenheit, "Fahrenheit = ", Celsius, " C"
